21336a2cccd1af29a0b0831cdac0740d58b89893
Diego Cortes <cortesmendez.2003@gmail.com>
2026-03-22 10:25:38 -0500

    Añadir migraciones para categorías, productos, imágenes de productos, atributos y variantes

--- backend/database/migrations/2026_03_22_055300_create_variant_values_table.php
+++ backend/database/migrations/2026_03_22_055300_create_variant_values_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('variant_values', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_variant_id')
+                ->constrained('product_variants')
+                ->cascadeOnDelete();
+            $table->foreignId('attribute_value_id')
+                ->constrained('attribute_values')
+                ->cascadeOnDelete();
             $table->timestamps();
         });
     }
